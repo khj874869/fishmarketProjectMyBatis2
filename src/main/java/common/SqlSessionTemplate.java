1396f2a6518925084ed743e5a8c8b67e885ad1e3
khj874869 <134674147+khj874869@users.noreply.github.com>
2023-08-07 09:09:07 +0900

    Update SqlSessionTemplate.java

--- src/main/java/common/SqlSessionTemplate.java
+++ src/main/java/common/SqlSessionTemplate.java
@@ -1,5 +1,28 @@
 package common;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class SqlSessionTemplate {
 
-}
+
+		public static SqlSession getSqlSession() {
+			String resource ="mybatis-config.xml";
+			SqlSession session = null;
+			try {
+				InputStream is = Resources.getResourceAsStream(resource);
+				SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+				SqlSessionFactory factory = builder.build(is);
+				session = factory.openSession();
+				factory.openSession();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return session;
+		}
+	}
